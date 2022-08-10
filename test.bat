1a23956e1275370b13250565a74f3c56787c8c24
Eve Polastri <106138177+CrazyCppChick@users.noreply.github.com>
2022-08-10 10:35:19 +0530

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,9 +2,4 @@
 echo Hello World from %AGENT_NAME%.
 echo My ID is %AGENT_ID%.
 echo AGENT_WORKFOLDER contents:
-@dir %AGENT_WORKFOLDER%
-echo AGENT_BUILDDIRECTORY contents:
-@dir %AGENT_BUILDDIRECTORY%
-echo BUILD_SOURCESDIRECTORY contents:
-@dir %BUILD_SOURCESDIRECTORY%
 echo Over and out.
